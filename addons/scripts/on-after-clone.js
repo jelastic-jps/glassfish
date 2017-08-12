104217c63e3cc905c36003d65bcab92b6e7ac7b0
Ruslan Synytsky <siruslan@gmail.com>
2017-08-13 00:40:14 +0200

    Update on-after-clone.js

--- addons/scripts/on-after-clone.js
+++ addons/scripts/on-after-clone.js
@@ -16,16 +16,6 @@ for (var i = 0, n = resp.nodes, l = n.length; i < l; i++) {
 }
 
 /**
-* updating links in index.html
-*/
-var cmd = "sed -i \"s/node" + oldDasId + "-" + oldEnvName + "/node" + dasId + "-" + envName + "/g\" ${STACK_PATH}/glassfish/domains/domain1/docroot/index.html";
-resp = cmdByGroup(cmd, "cp");
-if (resp.result != 0) return resp; else allResp.push(resp);
-
-resp = cmdByGroup(cmd, "das");
-if (resp.result != 0) return resp; else allResp.push(resp);
-
-/**
 * updating JMS host
 */
 cmd = "sed -i \"s/node" + oldDasId + "/node" + dasId + "/g\" ${STACK_PATH}/glassfish/domains/domain1/config/domain.xml";
@@ -38,13 +28,23 @@ if (resp.result != 0) return resp; else allResp.push(resp);
 resp = jelastic.env.control.RestartNodes(envName, session, "das", -1, -1);
 if (resp.result != 0) return resp; else allResp.push(resp);
 
+/**
+* updating links in index.html
+*/
+var cmd = "sed -i \"s/node" + oldDasId + "-" + oldEnvName + "/node" + dasId + "-" + envName + "/g\" ${STACK_PATH}/glassfish/domains/domain1/docroot/index.html";
+resp = cmdByGroup(cmd, "cp");
+if (resp.result != 0) return resp; else allResp.push(resp);
+
+resp = cmdByGroup(cmd, "das");
+if (resp.result != 0) return resp; else allResp.push(resp);
+
 
 /**
 * update redirect from "cp" master to DAS
 */
 cmd = ["d=com/sun/enterprise/v3/admin/adapter",
 "mkdir -p $d",
-"echo '<html><head><meta http-equiv=\"refresh\" content=\"0;url=https://node" + dasId + "-" + envName + ":4848/\" /></head></html>' > $d/statusNotDAS.html",
+"echo '<html><head><meta http-equiv=\"refresh\" content=\"0;url=https://node" + dasId + "-" + "${env.domain}".replace(oldEnvName, envName) + ":4848/\" /></head></html>' > $d/statusNotDAS.html",
 "jar uf $STACK_PATH/glassfish/modules/kernel.jar $d/statusNotDAS.html",
 "rm -rf com"].join("; ");
 
@@ -56,7 +56,6 @@ if (resp.result != 0) return resp; else allResp.push(resp);
 */
 resp = cmdByGroup("$STACK_PATH/service.sh clean", "das");
 if (resp.result != 0) return resp; else allResp.push(resp);
-
 
 /**
 * restart new nodes and register them in DAS
