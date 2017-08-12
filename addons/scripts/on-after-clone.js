9a214a6b43be764707447afab96848dd7ad55534
Ruslan Synytsky <siruslan@gmail.com>
2017-08-12 14:47:56 +0200

    Update on-after-clone.js

--- addons/scripts/on-after-clone.js
+++ addons/scripts/on-after-clone.js
@@ -1,10 +1,8 @@
 //@auth
 var oldEnvName = "${env.envName}",
     oldDasId = "${nodes.das.first.id}",
-    oldDasIP = "${nodes.das.first.address}",
     envName = "${event.response.env.envName}",
-    dasId, dasIP, cpMasterId, cpMasterIP, 
-    oldEnvCpMasterIP, allResp = [];
+    dasId, cpMasterId, allResp = [];
 
 var resp = jelastic.env.control.GetEnvInfo(envName, session);
 if (resp.result != 0) return resp; else allResp.push(resp);
@@ -13,57 +11,43 @@ if (resp.result != 0) return resp; else allResp.push(resp);
 for (var i = 0, n = resp.nodes, l = n.length; i < l; i++) {
     if (n[i].nodeGroup == 'das' && n[i].ismaster) {
         dasId = n[i].id;
-        dasIP = n[i].address;
     }
     if (n[i].nodeGroup == 'cp' && n[i].ismaster) {
         cpMasterId = n[i].id;
-        cpMasterIP = n[i].address;
     }
 }
 
 //updating links in index.html
-var params = "-i \"s/node" + oldDasId + "-" + oldEnvName + "/node" + dasId + "-" + envName + "/g\" ${STACK_PATH}/glassfish/domains/domain1/docroot/index.html";
-resp = cmdByGroup("sed", params, "cp");
+var cmd = "sed -i \"s/node" + oldDasId + "-" + oldEnvName + "/node" + dasId + "-" + envName + "/g\" ${STACK_PATH}/glassfish/domains/domain1/docroot/index.html";
+resp = cmdByGroup(cmd, "cp");
 if (resp.result != 0) return resp; else allResp.push(resp);
 
-resp = cmdByGroup("sed", params, "das");
+resp = cmdByGroup(cmd, "das");
 if (resp.result != 0) return resp; else allResp.push(resp);
 
 /**
 * clean old nodes in DAS
 */
-resp = cmdByGroup("$HOME_DIR/service.sh", "clean", "das");
+resp = cmdByGroup("$HOME_DIR/service.sh clean", "das");
 if (resp.result != 0) return resp; else allResp.push(resp);
 
 /**
-* update iptables forwarding from "cp" master to DAS
+* update redirect from "cp" master to DAS
 */
-resp = jelastic.env.control.GetEnvInfo(oldEnvName, session);
+cmd = "d=com/sun/enterprise/v3/admin/adapter; \
+mkdir -p $d; \
+echo '<html><head><meta http-equiv=\"refresh\" content=\"0;url=https://node" + dasId + "-" + envName + ":4848/\" /></head></html>' > $d/statusNotDAS.html; \
+jar uf ../glassfish/modules/kernel.jar $d/statusNotDAS.html; \
+rm -rf com";
+
+resp = cmdById(cmd, cpMasterId);
 if (resp.result != 0) return resp; else allResp.push(resp);
 
-for (var i = 0, n = resp.nodes, l = n.length; i < l; i++) {
-    if (n[i].nodeGroup == 'cp' && n[i].ismaster) {
-        oldEnvCpMasterIP = n[i].address;
-        break;
-    }
-}
-
-//clean old routing
-resp = preRouting(oldEnvCpMasterIP, oldDasIP, "D");
-if (resp.result != 0) return resp; else allResp.push(resp);
-resp = postRouting(oldEnvCpMasterIP, oldDasIP, "D");
-if (resp.result != 0) return resp; else allResp.push(resp);
-
-//add new routing
-resp = preRouting(cpMasterIP, dasIP, "A");
-if (resp.result != 0) return resp; else allResp.push(resp);
-resp = postRouting(cpMasterIP, dasIP, "A");
-if (resp.result != 0) return resp; else allResp.push(resp);
 
 /**
 * restart new nodes and register them in DAS
 */
-var resp = jelastic.env.control.RestartNodes(envName, session, "cp", -1, -1);
+resp = jelastic.env.control.RestartNodes(envName, session, "cp", -1, -1);
 if (resp.result != 0) return resp; else allResp.push(resp);
 
 return {
@@ -71,25 +55,15 @@ return {
     responses: allResp
 };
 
-  
-function preRouting(masterIP, dasIP, act){
-    return cmdById("iptables", "-t nat -" + act + " PREROUTING -p tcp -d " + masterIP + " --dport 4848 -j DNAT --to-destination " + dasIP, cpMasterId);
-}
 
-function postRouting(masterIP, dasIP, act){
-    return cmdById("iptables", "-t nat -" + act + " POSTROUTING -p tcp --dst " + dasIP + " --dport 4848 -j SNAT --to-source " + masterIP, cpMasterId);
-}
-
-function cmdById(cmd, params, nodeId) {
+function cmdById(cmd, nodeId) {
     return jelastic.env.control.ExecCmdById(envName, session, nodeId, toJSON([{
-        "command": cmd,
-        "params": params
+        "command": cmd
     }]), true, "root");
 }
 
-function cmdByGroup(cmd, params, group) {
+function cmdByGroup(cmd, group) {
     return jelastic.env.control.ExecCmdByGroup(envName, session, group, toJSON([{
-        "command": cmd,
-        "params": params
+        "command": cmd
     }]), true, false, "root");
 }
