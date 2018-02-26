188a138df599b65c86f059486ff04b86cf9cd51b
sych74 <sych74@gmail.com>
2018-02-26 17:11:55 +0100

    added more data to response with error

--- glassfish-cluster/scripts/add-triggers.js
+++ glassfish-cluster/scripts/add-triggers.js
@@ -1,8 +1,6 @@
 //@auth
 //@req(nodeGroup, resourceType, cleanOldTriggers, scaleUpValue, scaleUpLimit, scaleUpLoadPeriod, scaleDownValue, scaleDownLimit, scaleDownLoadPeriod)
-
-var nMaxSameNodes,
-    oResp;
+var nMaxSameNodes, oResp;
 
 oResp = jelastic.billing.account.GetQuotas('environment.maxsamenodescount').array[0];
 
@@ -16,62 +14,62 @@ if (nMaxSameNodes < scaleUpLimit) {
 
 if (cleanOldTriggers) {
     var actions = ['ADD_NODE', 'REMOVE_NODE'];
-    for (var i = 0; i < actions.length; i++){
+    for (var i = 0; i < actions.length; i++) {
         var array = jelastic.env.trigger.GetTriggers(appid, session, actions[i]).array;
-        for (var j = 0; j < array.length; j++) jelastic.env.trigger.DeleteTrigger(appid, session, array[j].id);          
+        for (var j = 0; j < array.length; j++) jelastic.env.trigger.DeleteTrigger(appid, session, array[j].id);
     }
 }
+var envName = '${env.envName}';
+var data = {
+    "isEnabled": true,
+    "name": "scale-up",
+    "nodeGroup": nodeGroup,
+    "period": scaleUpLoadPeriod,
+    "condition": {
+        "type": "GREATER",
+        "value": scaleUpValue,
+        "resourceType": resourceType,
+        "valueType": "PERCENTAGES"
+    },
+    "actions": [{
+        "type": "ADD_NODE",
+        "customData": {
+            "limit": scaleUpLimit,
+            "count": 1,
+            "notify": true
+        }
+    }]
+};
+resp = jelastic.env.trigger.AddTrigger(envName, session, data);
+if (resp.result != 0) {
+    resp.data = data;
+    return resp
+};
 
-resp = jelastic.env.trigger.AddTrigger('${env.envName}', session, 
-    {
-        "isEnabled": true,
-        "name": "scale-up",
-        "nodeGroup": nodeGroup,
-        "period": scaleUpLoadPeriod,
-        "condition": {
-            "type": "GREATER",
-            "value": scaleUpValue,
-            "resourceType": resourceType,
-            "valueType": "PERCENTAGES"
-        },
-        "actions": [
-            {
-                "type": "ADD_NODE",
-                "customData": {
-                    "limit": scaleUpLimit,
-                    "count": 1,
-                    "notify": true
-                }
-            }
-        ]
-    }
-);
-
-if (resp.result != 0) return resp;
-
-resp = jelastic.env.trigger.AddTrigger('${env.envName}', session,
-    {
-        "isEnabled": true,
-        "name": "scale-down",
-        "nodeGroup": nodeGroup,
-        "period": scaleDownLoadPeriod,
-        "condition": {
-            "type": "LESS",
-            "value": scaleDownValue,
-            "resourceType": resourceType,
-            "valueType": "PERCENTAGES"
-        },
-        "actions": [
-            {
-                "type": "REMOVE_NODE",
-                "customData": {
-                    "limit": scaleDownLimit,
-                    "count": 1,
-                    "notify": true
-                }
-            }
-        ]
-    }
-);
+data = {
+    "isEnabled": true,
+    "name": "scale-down",
+    "nodeGroup": nodeGroup,
+    "period": scaleDownLoadPeriod,
+    "condition": {
+        "type": "LESS",
+        "value": scaleDownValue,
+        "resourceType": resourceType,
+        "valueType": "PERCENTAGES"
+    },
+    "actions": [{
+        "type": "REMOVE_NODE",
+        "customData": {
+            "limit": scaleDownLimit,
+            "count": 1,
+            "notify": true
+        }
+    }]
+};
+resp = jelastic.env.trigger.AddTrigger(envName, session, data);
+if (resp.result != 0) {
+    resp.data = data;
+    return resp
+};
 
 return resp;
